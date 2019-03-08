57a6bbeeec09628eacda5300e57311a935392bb6
sylvartore <sylvartore@gmail.com>
2019-03-07 19:56:29 -0800

    Implements inline move

--- Board.java
+++ Board.java
@@ -1,5 +1,7 @@
 package sylvartore;
 
+import static sylvartore.Board.Direction.*;
+
 /**
  * ....I O O O O O
  * ...H O O O O O O
@@ -16,13 +18,22 @@ package sylvartore;
 
 public class Board {
 
-    public static int[][] transitionValue = {
+
+    public static int[][] transDown = {
             {0, -1},    //left
             {0, 1},     //right
             {-1, 0},    //left up
             {1, -1},    //left down
             {-1, 1},    // right up
             {1, 0},     // right down
+    };
+    public static int[][] transUp = {
+            {0, -1},    //left
+            {0, 1},     //right
+            {-1, -1},    //left up
+            {1, 0},    //left down
+            {-1, 0},    // right up
+            {1, 1},     // right down
     };
 
     enum Direction {Left, Right, LeftUp, LeftDown, RightUp, RightDown}
@@ -43,24 +54,94 @@ public class Board {
     }
 
     // 16 * 6 * 3 = 252
-    public void move(int row, int col, Direction d, int n) {
-
+    public boolean move(int row, int col, Direction d, int n) {
+        if (!isWithinBoard(row, col)) {
+            System.out.println("Invalid: origin out of bound");
+            return false;
+        }
+        if (state[row][col] == '+') {
+            System.out.println("nothing there");
+            return false;
+        }
+        int[] targetCor = transCell(row, col, d);
+        if (!isWithinBoard(targetCor[0], targetCor[1])) {
+            System.out.println("Invalid: suicide forbidden");
+            return false;
+        }
+        if (n == 1) {
+            if (state[targetCor[0]][targetCor[1]] == '+') {
+                basicMove(row, col, d);
+                return true;
+            }
+            return tryPush(row, col, d);
+        } else {
+            return true;
+        }
     }
 
-    public int getForce(int row, int col, char owner, int force, Direction d) {
-        if(force == 3) return 3;
-        if (state[row][col] != owner) return force;
-        return getForce(row + transitionValue[d.ordinal()][0], col + transitionValue[d.ordinal()][1],
-                owner, force + 1, d);
+    public boolean tryPush(int row, int col, Direction d) {
+        int force = 0;
+        int[] targetCor = transCell(row, col, d);
+        while (++force <= 3 && isWithinBoard(targetCor[0], targetCor[1]) && state[targetCor[0]][targetCor[1]] == state[row][col]) {
+            targetCor = transCell(targetCor[0], targetCor[1], d);
+        }
+        if (force > 3 || !isWithinBoard(targetCor[0], targetCor[1])) {
+            if (force > 3) {
+                System.out.println("Invalid: too much marble to push");
+                return false;
+            }
+            System.out.println("Invalid: suicide forbidden");
+            return false;
+        }
+        int[] counterForce = getF(targetCor[0], targetCor[1], d);
+        if (force > counterForce[2] && (!isWithinBoard(counterForce[0], counterForce[1])
+                || state[counterForce[0]][counterForce[1]] != state[row][col])) {
+            push(counterForce[0], counterForce[1], row, col, getCounterDirection(d));
+        } else {
+            if (force <= counterForce[2]) {
+                System.out.println("Invalid: not enough force");
+                return false;
+            }
+            System.out.println("Invalid: final place is not applicable");
+            return false;
+        }
+        return true;
     }
 
+    public void push(int toRow, int toCol, int fromRow, int fromCol, Direction d) {
+        int[] prev = new int[]{toRow, toCol};
+        int[] next = transCell(toRow, toCol, d);
+        while ((prev[0] == toRow && prev[1] == toCol || state[prev[0]][prev[1]] != '+') && (prev[0] != fromRow || prev[1] != fromCol)) {
+            if (isWithinBoard(prev[0], prev[1])) state[prev[0]][prev[1]] = state[next[0]][next[1]];
+            prev[0] = next[0];
+            prev[1] = next[1];
+            next = transCell(prev[0], prev[1], d);
+        }
+        state[fromRow][fromCol] = '+';
+    }
 
-    public void baseMove(int row, int col, Direction d) {
-        int targetRow = row + transitionValue[d.ordinal()][0];
-        int targetCol = col + transitionValue[d.ordinal()][1];
+    public int[] getF(int row, int col, Direction d) {
+        if (!isWithinBoard(row, col) || state[row][col] == '+') return new int[]{row, col, 0};
+        int force = 1;
+        int[] targetCor = transCell(row, col, d);
+        while (isWithinBoard(targetCor[0], targetCor[1]) && state[row][col] == state[targetCor[0]][targetCor[1]] && ++force <= 3) {
+            targetCor = transCell(targetCor[0], targetCor[1], d);
+        }
+        if (state[targetCor[0]][targetCor[1]] != '+') targetCor = transCell(targetCor[0], targetCor[1], d);
+        return new int[]{targetCor[0], targetCor[1], force == 4 ? 0 : force};
+    }
+
+    public void basicMove(int row, int col, Direction d) {
+        int[] targetCor = transCell(row, col, d);
         char temp = state[row][col];
-        state[row][col] = state[targetRow][targetCol];
-        state[targetRow][targetCol] = temp;
+        state[row][col] = state[targetCor[0]][targetCor[1]];
+        state[targetCor[0]][targetCor[1]] = temp;
+    }
+
+    public int[] transCell(int row, int col, Direction d) {
+        int targetRow = row + transDown[d.ordinal()][0];
+        if (targetRow > 4 || targetRow == 4 && row > 4) return new int[]{targetRow, col + transDown[d.ordinal()][1]};
+        return new int[]{targetRow, col + transUp[d.ordinal()][1]};
     }
 
     public void print(boolean testMode) {
@@ -80,12 +161,17 @@ public class Board {
             spaces += decre ? -1 : 1;
             if (spaces == 0) decre = false;
         }
+        System.out.println();
+    }
+
+    public void print() {
+        print(false);
     }
 
     private void fillRow(int row) {
         char marble = '+';
         if (row >= 0 && row <= 2 || row >= rows - 3 && row <= rows - 1) marble = row <= 2 ? '@' : 'O';
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < rowToCol(row); i++) {
             state[row][i] = marble;
         }
         if (row == 2 || row == rows - 3) {
@@ -93,6 +179,46 @@ public class Board {
             state[row][1] = '+';
             state[row][5] = '+';
             state[row][6] = '+';
+        }
+    }
+
+    private boolean isWithinBoard(int row, int col) {
+        return row >= 0 && row < rows && col >= 0 && col < rowToCol(row);
+    }
+
+    private Direction getCounterDirection(Direction direction) {
+        switch (direction) {
+            case Left:
+                return Right;
+            case Right:
+                return Left;
+            case LeftUp:
+                return RightDown;
+            case LeftDown:
+                return RightUp;
+            case RightDown:
+                return LeftUp;
+            case RightUp:
+                return LeftDown;
+        }
+        return null;
+    }
+
+    public static int rowToCol(int row) {
+        switch (row) {
+            case 4:
+                return 9;
+            case 3:
+            case 5:
+                return 8;
+            case 2:
+            case 6:
+                return 7;
+            case 1:
+            case 7:
+                return 6;
+            default:
+                return 5;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -3,12 +3,50 @@ package sylvartore;
 public class Main {
 
     public static void main(String[] args) {
-        Board b = new Board();
-        b.print(true);
-        System.out.println();
-        b.print(false);
+        run();
+    }
 
-        b.print(true);
+    public static void run() {
+        Board b = new Board();
+        b.move(8, Board.rowToCol(8) - 1, Board.Direction.LeftUp, 1);
+        b.print();
+        b.move(0, 0, Board.Direction.RightDown, 1);
+        b.print();
+        b.move(8, Board.rowToCol(8) - 2, Board.Direction.LeftUp, 1);
+        b.print();
+        b.move(0, 1, Board.Direction.RightDown, 1);
+        b.print();
+        b.move(8, 2, Board.Direction.RightUp, 1);
+        b.print();
+        b.move(1, 2, Board.Direction.RightDown, 1);
+        b.print();
+        b.move(7, 2, Board.Direction.RightUp, 1);
+        b.move(6, 3, Board.Direction.RightUp, 1);
+        b.print();
+        b.move(5, 4, Board.Direction.RightUp, 1);
+        b.print();
+    }
+
+    void testCase(Board b) {
+        int[] targetCor;
+        // System.out.println(b.getForce(7,5, Board.Direction.LeftDown));
+        b.basicMove(1, 5, Board.Direction.LeftDown);
+        targetCor = b.transCell(1, 5, Board.Direction.LeftDown);
+        b.basicMove(targetCor[0], targetCor[1], Board.Direction.RightDown);
+        targetCor = b.transCell(targetCor[0], targetCor[1], Board.Direction.RightDown);
+        b.basicMove(targetCor[0], targetCor[1], Board.Direction.LeftDown);
+        targetCor = b.transCell(targetCor[0], targetCor[1], Board.Direction.LeftDown);
+        b.basicMove(targetCor[0], targetCor[1], Board.Direction.RightDown);
+        targetCor = b.transCell(targetCor[0], targetCor[1], Board.Direction.RightDown);
+        b.basicMove(targetCor[0], targetCor[1], Board.Direction.LeftDown);
+        //  targetCor = b.transCell(targetCor[0], targetCor[1], Board.Direction.LeftDown);
+        //   b.basicMove(targetCor[0], targetCor[1], Board.Direction.RightDown);
+//        b.push(6,6,Board.Direction.Left);
+//        b.push(6,7,Board.Direction.Left);
+        b.print(false);
+        b.move(6, 2, Board.Direction.Right, 1);
+        b.move(6, 4, Board.Direction.Right, 1);
+        b.move(6, 5, Board.Direction.Right, 1);
         System.out.println();
         b.print(false);
     }
